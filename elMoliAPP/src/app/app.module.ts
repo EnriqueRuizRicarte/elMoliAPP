f783eb4cf385d89e109b86e43d6a7dd5f7de2a80
Enrique Ruiz Ricarte <enrique.ruiz.ricarte@cieep.com>
2019-04-08 21:53:22 +0200

    Pre configurado la conexion a firebase

--- elMoliAPP/src/app/app.module.ts
+++ elMoliAPP/src/app/app.module.ts
@@ -9,19 +9,42 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Angular FireBase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBMD06uvsgPTd31LAUmHtMrPTAM3MdeuTY",
+  authDomain: "fallaelmoli-3fdb8.firebaseapp.com",
+  databaseURL: "https://fallaelmoli-3fdb8.firebaseio.com",
+  projectId: "fallaelmoli-3fdb8",
+  storageBucket: "fallaelmoli-3fdb8.appspot.com",
+  messagingSenderId: "727082792199"
+};
+
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
+  declarations: [
+    AppComponent
+  ],
+
+  entryComponents: [
+
+  ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- elMoliAPP/src/app/home/home.page.ts
+++ elMoliAPP/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NavController } from '@ionic/angular';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  items: Observable<any[]>;
+  constructor(public navCtrl: NavController, public afDB: AngularFireDatabase) {
+    this.items = this.afDB.list('noticias').valueChanges();
+  }
 
 }
