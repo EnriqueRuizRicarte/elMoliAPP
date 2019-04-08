0a47f5d3d0b6b39ddc28f4c1ed4a9d75f1687a97
Enrique Ruiz Ricarte <enrique.ruiz.ricarte@cieep.com>
2019-04-08 20:22:39 +0200

    Estructura de ficheros para la entidad de evento

--- elMoliAPP/src/app/app.component.ts
+++ elMoliAPP/src/app/app.component.ts
@@ -11,12 +11,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
+      title: 'Tablón',
       url: '/home',
       icon: 'home'
     },
     {
-      title: 'List',
+      title: 'Eventos',
       url: '/list',
       icon: 'list'
     }
