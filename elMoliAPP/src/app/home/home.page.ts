a0201b14a857dbd666f1880948585ad55c50deaf
Enrique Ruiz Ricarte <enrique.ruiz.ricarte@cieep.com>
2019-04-09 22:55:58 +0200

    Configuracion del GetNoticias de firebase

--- elMoliAPP/src/app/home/home.page.ts
+++ elMoliAPP/src/app/home/home.page.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 import { NavController } from '@ionic/angular';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { FirebaseFirestore } from '@angular/fire';
+import { NoticiaService } from '../providers/noticia.service';
+import { Noticia } from './noticia.model';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +13,30 @@ import { FirebaseFirestore } from '@angular/fire';
 })
 export class HomePage {
 
-  items: any;
-  constructor(public navCtrl: NavController, public afDB:FirebaseFirestore) {
-     this.items = this.afDB.collection('noticias').get();
-     console.log(this.items);
+  noticias: Noticia[];
+  constructor(public navCtrl: NavController, private _noticiaService: NoticiaService) {
+    this.getNoticias();
+  }
+
+  doRefresh(event) {
+    //INICIO DEL REFESH
+    setTimeout(()=>{
+      //Obtenemos las noticias de nuevo por si hay algun cambio
+      this.getNoticias();
+      //Fin del refresh
+      event.complete();
+    },1000);
+  }
+
+  getNoticias(){
+    this._noticiaService.getNoticias().subscribe(
+      res => {
+        this.noticias = res;
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
 }
